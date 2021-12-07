50267f7df7a4007d2d424315854684fecf7c9bab
Mashudu Ralephata <Mashudu.Ralephata@outlook.com>
2021-12-07 02:37:08 +0200

    first commit

---
+++ 30DayChallenge/day05.js
@@ -0,0 +1,7 @@
+//Version 01
+var inputNumber=2;
+for (let index = 0; index < 11; index++) 
+{
+    var operation=inputNumber*index;
+    console.log(inputNumber + " x " + index+" = "+ operation);
+}
